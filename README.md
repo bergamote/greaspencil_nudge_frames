26779eb03289b1f8979a47917dc11e46d2b5ec5b
bergamote <wilks@mraro.com>
2020-05-17 21:31:02 +0100

    removed comments, added to readme

--- README.md
+++ README.md
@@ -1,12 +1,19 @@
-Greasepencil Nudge Frames
-=========================
+# Greasepencil Nudge Frames
 
-This is very simple Add-on for [Blender](http://blender.org), to speed up modifying the length of Greasepencil frames. It adds two keyboard shortcuts to the Dopesheet, `D` to shorten and `F` to extend, the currently displayed keyframe.
 
-Pressing `D` does the same thing as typing `[`, `Ctrl+I`, `G`, `-`, `1` and `Enter`.
+This is very simple Add-on for [Blender](http://blender.org), to speed up modifying the length of Greasepencil frames.  
+It adds two keyboard shortcuts to the Dopesheet, `D` to shorten and `F` to extend, the duration of the currently displayed keyframe by 1 frame.
+
+Pressing `D` does the same thing as typing `[`, `Ctrl+I`, `G`, `-`, `1` and `Enter`.  
 Pressing `F` does the same thing as typing `[`, `Ctrl+I`, `G`, `1` and `Enter`.
 
 To install it, got to `Edit > Preferences > Add-ons > Install...`, select `greasepencil_nudge_frames.py` and confirm, then enable it by ticking the checkbox.
 
-**Note:** Pressing `D` is a destructive operation! If the playhead is on top of a keyframe, it will get shortened until the next keyframe along ends up taking its place. 
+
+### Notes
+
+- Pressing `D` is a **destructive** operation! If the playhead is on top of a keyframe, it will get shortened until the next keyframe along ends up taking its place, erasing it in the process. 
+- The shortcuts move the frames from **every** layers, so make sure you lock the layers that you to keep in place.
+- This is my first add-on for Blender, so most likely badly written and possibly buggy Use at your own risks and all that.
+
 
--- greasepencil_nudge_frames.py
+++ greasepencil_nudge_frames.py
@@ -2,7 +2,8 @@ bl_info = {
     "name": "Nudge Grease Pencil Frames",
     "Description": "Shorten (D) or Extend (F) the duration of the current Grease Pencil frame",
     "blender": (2, 80, 0),
-    "category": "Animation"
+    "category": "Animation",
+    "author": "bergamote"
 }
 
 import bpy
@@ -14,19 +15,19 @@ def selectFrames():
 
 
 class ExtendGpFrame(bpy.types.Operator):
-    """Extend the current Grease Pencil keyframe"""      # Use this as a tooltip for menu items and buttons.
-    bl_idname = "action.extend_gp_frame"        # Unique identifier for buttons and menu items to reference.
-    bl_label = "Extend frame"         # Display name in the interface.
-    bl_options = {'REGISTER', 'UNDO'}  # Enable undo for the operator.
+    """Extend the current Grease Pencil keyframe"""
+    bl_idname = "action.extend_gp_frame" menu items to reference.
+    bl_label = "Extend frame"
+    bl_options = {'REGISTER', 'UNDO'} 
 
 
-    def execute(self, context):        # execute() is called when running the operator.
+    def execute(self, context): 
         if bpy.context.area.type == 'DOPESHEET_EDITOR':
             selectFrames()
             bpy.ops.transform.transform(mode='TIME_TRANSLATE', value=(1,0,0,0))
 
-        return {'FINISHED'}            # Lets Blender know the operator finished successfully.
-
+        return {'FINISHED'} 
+        
 class ShortenGpFrame(bpy.types.Operator):
     """Shorten the current Grease Pencil keyframe"""
     bl_idname = "action.shorten_gp_frame"
@@ -40,16 +41,13 @@ class ShortenGpFrame(bpy.types.Operator):
 
         return {'FINISHED'}
 
-# store keymaps here to access after registration
+
 addon_keymaps = []
 
 def register():
     bpy.utils.register_class(ExtendGpFrame)
     bpy.utils.register_class(ShortenGpFrame)
-    # handle the keymap
     wm = bpy.context.window_manager
-    # Note that in background mode (no GUI available), keyconfigs are not available either,
-    # so we have to check this to avoid nasty errors in background case.
     kc = wm.keyconfigs.addon
     if kc:
         km = wm.keyconfigs.addon.keymaps.new(name='Window', space_type='EMPTY')
@@ -58,7 +56,6 @@ def register():
         kmi = km.keymap_items.new(ShortenGpFrame.bl_idname, 'D', 'PRESS')
         addon_keymaps.append((km, kmi))
 
-
 def unregister():
     for km, kmi in addon_keymaps:
         km.keymap_items.remove(kmi)
@@ -66,7 +63,8 @@ def unregister():
     bpy.utils.unregister_class(ExtendGpFrame)
     bpy.utils.unregister_class(ShortenGpFrame)
 
-# This allows you to run the script directly from Blender's Text editor
-# to test the add-on without having to install it.
 if __name__ == "__main__":
     register()
+    
+
+# Based on the tutorial at https://docs.blender.org/manual/en/latest/advanced/scripting/addon_tutorial.html
